a24c6c7672f7be1faf8475f6c17f5f26efdc0985
VieuxLoupGris <provocemois@gmail.com>
2019-01-24 16:29:13 +0100

    Class rectangle create and start of method otherRectangle

---
+++ Exercice_6.2.js
@@ -0,0 +1,12 @@
+class Rectangle {
+  constructor(topLeftXPos, topLeftYPos, width, lenght) {
+    this.topLeftXPos = topLeftXPos;
+    this.topLeftYPos = topLeftYPos;
+    this.width = width;
+    this.length = lenght;
+  }
+
+  collides(otherRectangle) {
+    
+  }
+}

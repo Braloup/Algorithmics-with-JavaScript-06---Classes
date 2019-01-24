fd0bd217b8d113ae1aeab377cd42d5566a05717e
Emmanuel Goldstein <30625064+VieuxLoupGris@users.noreply.github.com>
2019-01-24 16:18:15 +0100

    Create an objet, a method and a setter

---
+++ Exercice_6.1.js
@@ -0,0 +1,24 @@
+class Circle {
+  constructor(xPos, yPos, radius) {
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.radius = radius;
+  }
+
+  move(xOffset, yOffset) {
+
+    console.log("Actaul x position is: " + this.xPos + " Actual y position is " + this.yPos);
+    console.log(" The new x postion after move is: " + (this.xPos + xOffset) + " The new y postion after move is: " + (this.yPos + yOffset));
+
+  }
+
+  get surface() {
+    return (Math.pow(this.radius, 2) * 3.1415).toFixed(3);
+  }
+
+}
+
+let circle1 = new Circle(120, 240, 35).move(100, 120);
+
+let circle2 = new Circle(120, 140, 15).surface;
+console.log(circle2);

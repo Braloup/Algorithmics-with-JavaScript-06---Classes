4a9c3366e249351757bb0b295aa3f0f143f503f1
VieuxLoupGris <provocemois@gmail.com>
2019-01-24 16:45:50 +0100

    Propely method with a return

--- Exercice_6.1.js
+++ Exercice_6.1.js
@@ -7,18 +7,18 @@ class Circle {
 
   move(xOffset, yOffset) {
 
-    console.log("Actaul x position is: " + this.xPos + " Actual y position is " + this.yPos);
-    console.log(" The new x postion after move is: " + (this.xPos + xOffset) + " The new y postion after move is: " + (this.yPos + yOffset));
+    return "Actaul x position is: " + this.xPos + " Actual y position is " + this.yPos +
+    " The new x postion after move is: " + (this.xPos + xOffset) + " The new y postion after move is: " + (this.yPos + yOffset);
 
   }
 
   get surface() {
-    return (Math.pow(this.radius, 2) * 3.1415).toFixed(3);
+    return Math.pow(this.radius, 2) * 3.1415;
   }
 
 }
 
 let circle1 = new Circle(120, 240, 35).move(100, 120);
-
+console.log(circle1);
 let circle2 = new Circle(120, 140, 15).surface;
 console.log(circle2);

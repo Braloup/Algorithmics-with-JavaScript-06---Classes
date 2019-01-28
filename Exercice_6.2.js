008f8d4cc945163303acbc7fa3baa33e6b7cb9fc
VieuxLoupGris <provocemois@gmail.com>
2019-01-28 12:53:17 +0100

    Add comment

--- Exercice_6.2.js
+++ Exercice_6.2.js
@@ -1,13 +1,13 @@
 /*
-**This code is a class with a constructor who make a 2D rectanglle with a constructor .
-**The class have a method who tcheck if two rectangle collide or not.
-**If he collide, the method return true.
-**Else, the method return "No collision detected".
-*/
+ **This code is a class with a constructor who make a 2D rectanglle with a constructor .
+ **The class have a method who tcheck if two rectangle collide or not.
+ **If he collide, the method return true.
+ **Else, the method return "No collision detected".
+ */
 
 class Rectangle {
 
-  constructor(topLeftXPos, topLeftYPos, width, lenght) {
+  constructor(topLeftXPos, topLeftYPos, width, lenght) { //Constructor who make a rectangle.
     this.topLeftXPos = topLeftXPos;
     this.topLeftYPos = topLeftYPos;
     this.width = width;
@@ -16,7 +16,7 @@ class Rectangle {
 
   collides(otherRectangle) {
 
-    if (this.topLeftXPos < otherRectangle.topLeftXPos + otherRectangle.width &&
+    if (this.topLeftXPos < otherRectangle.topLeftXPos + otherRectangle.width && // This code verify the collission between two rectangle.
       this.topLeftXPos + this.width > otherRectangle.topLeftXPos &&
       this.topLeftYPos < otherRectangle.topLeftYPos + otherRectangle.length &&
       this.length + this.topLeftYPos > otherRectangle.topLeftYPos) {
@@ -28,10 +28,10 @@ class Rectangle {
   }
 
 }
-let rectangle1 = new Rectangle(25, 20, 150, 220);
-let rectangle2 = new Rectangle(385, 375, 100, 180).collides(rectangle1);
+let rectangle1 = new Rectangle(25, 20, 150, 220); // Create rectangle1.
+let rectangle2 = new Rectangle(385, 375, 100, 180).collides(rectangle1); // Create rectangle2 and verify if is collide or not with rectangle1.
 console.log(rectangle2); //Return no collision detected.
 
-let rectangle3 = new Rectangle(25, 20, 150, 220);
-let rectangle4 = new Rectangle(45, 65, 100, 180).collides(rectangle3);
+let rectangle3 = new Rectangle(25, 20, 150, 220); // Create rectangle3.
+let rectangle4 = new Rectangle(45, 65, 100, 180).collides(rectangle3); // Create rectangle2 and verify if is collide or not with rectangle1.
 console.log(rectangle4); //Return true

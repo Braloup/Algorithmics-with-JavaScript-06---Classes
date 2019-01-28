b62676a79121e20340a9798cc46d6a1f70fec720
VieuxLoupGris <provocemois@gmail.com>
2019-01-28 11:58:13 +0100

    fonctionnal running collision test for rectangle in 2D

--- Exercice_6.2.js
+++ Exercice_6.2.js
@@ -9,9 +9,22 @@ class Rectangle {
 
   collides(otherRectangle) {
 
-  
-    // collision détectée !
-}
+    if (this.topLeftXPos < otherRectangle.topLeftXPos + otherRectangle.width &&
+      this.topLeftXPos + this.width > otherRectangle.topLeftXPos &&
+      this.topLeftYPos < otherRectangle.topLeftYPos + otherRectangle.length &&
+      this.length + this.topLeftYPos > otherRectangle.topLeftYPos) {
+      return true;     // collision détectée !
+    } else {
+      return "No collision detected";
+    }
 
   }
+
 }
+let rectangle1 = new Rectangle(25, 20, 150, 220);
+let rectangle2 = new Rectangle(385, 375, 100, 180).collides(rectangle1);
+console.log(rectangle2);  //Return no collision detected.
+
+let rectangle3 = new Rectangle(25, 20, 150, 220);
+let rectangle4 = new Rectangle(45, 65, 100, 180).collides(rectangle3);
+console.log(rectangle4);  //Return true
